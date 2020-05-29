8b25cb36abb67636226c2a50ff6ef1f8c7f751d1
Alexander Steele <alexander.steele@and.digital>
2020-05-29 09:01:52 +0100

    tidied up dashboard demo

--- src/components/Demos/DemoDashboard/DemoDashboard.jsx
+++ src/components/Demos/DemoDashboard/DemoDashboard.jsx
@@ -5,73 +5,24 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import '../../../../node_modules/react-vis/dist/style.css'
-import {FlexibleWidthXYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, RadialChart, makeVisFlexible, DiscreteColorLegend} from 'react-vis';
-import { useCardStyles } from '../../../hooks'
+import { FlexibleWidthXYPlot, LineSeries, VerticalGridLines, 
+         HorizontalGridLines, XAxis, YAxis, RadialChart, makeVisFlexible, 
+         DiscreteColorLegend} from 'react-vis';
+import { useCardStyles } from '../../../hooks';
+import dashboardData from './dashboardData';
 
 const DemoDashboard = () => {
 
     const classes = useCardStyles();
-
     const FlexRadialChart = makeVisFlexible(RadialChart)
-
-    const data = [
-        {x: new Date("2010"), y: 3},
-        {x: new Date("2011"), y: 1},
-        {x: new Date("2012"), y: 2},
-        {x: new Date("2013"), y: 0},
-        {x: new Date("2014"), y: 2},
-        {x: new Date("2015"), y: 1},
-        {x: new Date("2016"), y: 3},
-        {x: new Date("2017"), y: 0},
-        {x: new Date("2018"), y: 10},
-        {x: new Date("2019"), y: 0},
-        {x: new Date("2020"), y: 1}
-      ];
-
-      const series = ["Shark Fatalaties(240)", "Spontaneous Combustion Fatalaties(2)"]
-
-      const series2 = ["USA", "Rest of World"]
-
-      const data2 = [{angle: 2}, {angle: 240}]
-
-      const usa_data = [
-        {x: new Date("2010"), y: 2},
-        {x: new Date("2011"), y: 0},
-        {x: new Date("2012"), y: 0},
-        {x: new Date("2013"), y: 0},
-        {x: new Date("2014"), y: 1},
-        {x: new Date("2015"), y: 1},
-        {x: new Date("2016"), y: 2},
-        {x: new Date("2017"), y: 0},
-        {x: new Date("2018"), y: 8},
-        {x: new Date("2019"), y: 0},
-        {x: new Date("2020"), y: 1}
-      ];
-
-      const rest_data = [
-        {x: new Date("2010"), y: 1},
-        {x: new Date("2011"), y: 1},
-        {x: new Date("2012"), y: 2},
-        {x: new Date("2013"), y: 0},
-        {x: new Date("2014"), y: 1},
-        {x: new Date("2015"), y: 1},
-        {x: new Date("2016"), y: 1},
-        {x: new Date("2017"), y: 0},
-        {x: new Date("2018"), y: 2},
-        {x: new Date("2019"), y: 0},
-        {x: new Date("2020"), y: 0}
-      ];
 
     return (
 
-
-        
         <div>
             <Typography variant="h4" component="h1">
                 Spontaneous Combustion Data Dashboard (FAKE DATA)
             </Typography>
             <br />
-
 
             <Grid container className={classes.root} spacing={2}>
                 <Grid item xs={12} lg={8}>
@@ -86,7 +37,7 @@ const DemoDashboard = () => {
                                     <HorizontalGridLines />
                                     <XAxis title="Year"/>
                                     <YAxis title="Deaths recorded as Spontaneous Combustion"/>
-                                    <LineSeries data={data} />
+                                    <LineSeries data={dashboardData.data} />
                                 </FlexibleWidthXYPlot>
 
                             </CardContent>
@@ -100,11 +51,8 @@ const DemoDashboard = () => {
                                 <Typography variant="h5" component="h2">
                                     Spon. Combustion vs. Sharks AVG
                                 </Typography>
-                                <DiscreteColorLegend  items={series}/>
-
-                                <FlexRadialChart data={data2} height={200}/>
-
-
+                                <DiscreteColorLegend  items={dashboardData.series}/>
+                                <FlexRadialChart data={dashboardData.data2} height={200}/>
                             </CardContent>
                         </Card>
                     </Box>
@@ -120,8 +68,6 @@ const DemoDashboard = () => {
                                 <Typography variant="h1" component="h1" style={{textAlign: "center"}}>
                                     3
                                 </Typography>
-
-
                             </CardContent>
                         </Card>
                     </Box>
@@ -136,7 +82,7 @@ const DemoDashboard = () => {
 
                                 <Grid container className={classes.root} spacing={2}>
                                 <Grid item xs={2} lg={2}>
-                                    <DiscreteColorLegend  items={series2}/>
+                                    <DiscreteColorLegend  items={dashboardData.series2}/>
                                 </Grid>
                                 <Grid item xs={10} lg={10}>
                                     <FlexibleWidthXYPlot xType="time" height={280}>
@@ -144,8 +90,8 @@ const DemoDashboard = () => {
                                         <HorizontalGridLines />
                                         <XAxis title="Year"/>
                                         <YAxis title="Spontaneous Combustion Cases"/>
-                                        <LineSeries data={usa_data} />
-                                        <LineSeries data={rest_data} />
+                                        <LineSeries data={dashboardData.usa_data} />
+                                        <LineSeries data={dashboardData.rest_data} />
                                     </FlexibleWidthXYPlot>
                                 </Grid>
                                 
---
+++ src/components/Demos/DemoDashboard/dashboardData.js
@@ -0,0 +1,49 @@
+const dashboardData = {
+    data: [
+        {x: new Date("2010"), y: 3},
+        {x: new Date("2011"), y: 1},
+        {x: new Date("2012"), y: 2},
+        {x: new Date("2013"), y: 0},
+        {x: new Date("2014"), y: 2},
+        {x: new Date("2015"), y: 1},
+        {x: new Date("2016"), y: 3},
+        {x: new Date("2017"), y: 0},
+        {x: new Date("2018"), y: 10},
+        {x: new Date("2019"), y: 0},
+        {x: new Date("2020"), y: 1}
+      ],
+      series: ["Shark Fatalaties(240)", "Spontaneous Combustion Fatalaties(2)"],
+      series2: ["USA", "Rest of World"],
+
+      data2: [{angle: 2}, {angle: 240}],
+
+      usa_data: [
+        {x: new Date("2010"), y: 2},
+        {x: new Date("2011"), y: 0},
+        {x: new Date("2012"), y: 0},
+        {x: new Date("2013"), y: 0},
+        {x: new Date("2014"), y: 1},
+        {x: new Date("2015"), y: 1},
+        {x: new Date("2016"), y: 2},
+        {x: new Date("2017"), y: 0},
+        {x: new Date("2018"), y: 8},
+        {x: new Date("2019"), y: 0},
+        {x: new Date("2020"), y: 1}
+      ],
+
+      rest_data: [
+        {x: new Date("2010"), y: 1},
+        {x: new Date("2011"), y: 1},
+        {x: new Date("2012"), y: 2},
+        {x: new Date("2013"), y: 0},
+        {x: new Date("2014"), y: 1},
+        {x: new Date("2015"), y: 1},
+        {x: new Date("2016"), y: 1},
+        {x: new Date("2017"), y: 0},
+        {x: new Date("2018"), y: 2},
+        {x: new Date("2019"), y: 0},
+        {x: new Date("2020"), y: 0}
+      ]
+}
+
+export default dashboardData;
